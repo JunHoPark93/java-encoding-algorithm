8e563a871e825ab60f9e3aa58bcba4459f27cb7a
pjh374968@gmail.com <pjh374968@gmail.com>
2018-12-31 09:50:22 +0900

    Initial commit

---
+++ src/main/java/com/nexters/javaencodingalgorithm/JavaEncodingAlgorithmApplication.java
@@ -0,0 +1,14 @@
+package com.nexters.javaencodingalgorithm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaEncodingAlgorithmApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JavaEncodingAlgorithmApplication.class, args);
+    }
+
+}
+
