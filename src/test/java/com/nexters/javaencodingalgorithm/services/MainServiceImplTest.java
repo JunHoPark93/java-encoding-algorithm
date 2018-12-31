af5b02aee90070e1445f15d0ceec5ca0808ed591
pjh374968@gmail.com <pjh374968@gmail.com>
2018-12-31 16:37:31 +0900

    Create test for main service

---
+++ src/test/java/com/nexters/javaencodingalgorithm/services/MainServiceImplTest.java
@@ -0,0 +1,31 @@
+package com.nexters.javaencodingalgorithm.services;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import java.util.Base64;
+
+import static org.junit.Assert.assertEquals;
+
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class MainServiceImplTest {
+
+    @Value("${urls.sample1}")
+    String url;
+
+    @Test
+    public void encodeStringWithBase64() {
+        String base64MatchPattern = "^([A-Za-z0-9+/]{4})*([A-Za-z0-9+/]{3}=|[A-Za-z0-9+/]{2}==)?$";
+
+        byte[] targetBytes = url.getBytes();
+        Base64.Encoder encoder = Base64.getEncoder();
+
+        String s = new String(encoder.encode(targetBytes));
+        assertEquals(true, s.matches(base64MatchPattern));
+    }
+}
